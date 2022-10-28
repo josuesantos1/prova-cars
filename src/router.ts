b8aba9633d1a00aebe6df117862f320259084a1c
josue santos <josue.santos.dev@gmail.com>
2022-10-28 18:52:48 -0300

    [app] (routes) users routes; post and get

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { appEndpoint } from "./endpoints/app";
+import { UsersHandler } from "./handlers/users";
 
 const router: Router = Router()
 
 //Routes
 router.get("/", appEndpoint.home);
+
+//users routes
+router.post("/users", new UsersHandler().create);
+router.get("/users/:user", new UsersHandler().get);
 
 export { router };
