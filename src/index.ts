5400c340d50735ef7636835c60284337dfb50098
josue santos <josue.santos.dev@gmail.com>
2022-10-28 18:53:42 -0300

    [app] (index) init express after data-source initialized

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { App } from "./app"
+import { AppDataSource } from "./data-source"
 
-new App().server.listen(3000);
+AppDataSource.initialize()
+    .then(() => {
+        console.log("[{+}] AppResource initialized")
+        new App().server.listen(3000);
+    })
+    .catch((error) => console.log(error))
