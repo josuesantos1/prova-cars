2f8f3ece53b1db15d204114c034ab7871689ba7d
josue santos <josue.santos.dev@gmail.com>
2022-10-28 21:06:46 -0300

    [app] (routes) cars routes

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { appEndpoint } from "./endpoints/app";
+import { CarsHandler } from "./handlers/cars";
 import { UsersHandler } from "./handlers/users";
 
 const router: Router = Router()
@@ -12,5 +13,12 @@ router.post("/users", new UsersHandler().create);
 router.get("/users/:user", new UsersHandler().get);
 router.put("/users/:user", new UsersHandler().update);
 router.delete("/users/:user", new UsersHandler().delete);
+
+
+//cars routes
+router.get("/cars", new CarsHandler().create);
+router.get("/cars/:user", new CarsHandler().get);
+router.put("/cars/:user", new CarsHandler().update);
+router.delete("/cars/:user", new CarsHandler().delete);
 
 export { router };
