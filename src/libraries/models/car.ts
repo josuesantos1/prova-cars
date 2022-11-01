1cb9a22f93d61f3221aa809e42312f3258f9e1ee
Josué Santos <josue.santos.dev@gmail.com>
2022-11-01 14:23:03 -0300

    [models] (cars) schema and validate

--- src/libraries/models/car.ts
+++ src/libraries/models/car.ts
@@ -1,6 +1,14 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToOne, ManyToOne, JoinColumn } from "typeorm"
+import * as yup from 'yup';
 import { User } from "./user";
 
+
+export const schema = yup.object().shape({
+    title: yup.string().required(),
+    model: yup.string().required(),
+    description: yup.string().required(),
+    price: yup.number().required()
+});
 
 @Entity("cars")
 export class Car {
@@ -9,6 +17,9 @@ export class Car {
 
     @Column()
     title: string;
+
+    @Column()
+    model: string;
 
     @Column({type: 'text'})
     description: string;
