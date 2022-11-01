4c5cd918fd2f90ac73418ec50c49d1266462d770
Josué Santos <josue.santos.dev@gmail.com>
2022-11-01 12:38:22 -0300

    [app] (routes) cars listing routes; remove users params from users routes

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import path from "path";
+import multer from 'multer'
+import crypto from 'crypto';
+
 import { appEndpoint } from "./endpoints/app";
 import { CarsHandler } from "./handlers/cars";
+import { FilesHandler } from "./handlers/files";
 import { UsersHandler } from "./handlers/users";
 import { Auth } from "./libraries/middlewares/auth";
+// import { upload } from "./libraries/middlewares/upload";
 
 const router: Router = Router()
 const authMiddle = new Auth();
@@ -16,6 +22,7 @@ router.post("/signin", new UsersHandler().login);
 
 //cars routes
 router.get("/cars/:car", new CarsHandler().get);
+router.get("/cars", new CarsHandler().listing);
 
 // authorization
 router.use(authMiddle.authorization)
@@ -23,9 +30,9 @@ router.post("/cars", new CarsHandler().create);
 router.put("/cars/:car", new CarsHandler().update);
 router.delete("/cars/:car", new CarsHandler().delete);
 
-router.get("/users/:user", new UsersHandler().get);
-router.put("/users/:user", new UsersHandler().update);
-router.delete("/users/:user", new UsersHandler().delete);
+router.get("/users", new UsersHandler().get);
+router.put("/users", new UsersHandler().update);
+router.delete("/users", new UsersHandler().delete);
 
 
 export { router };
