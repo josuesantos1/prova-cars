527ec09bd042614c669e0af0be1c9cad13bbafd4
Josué Santos <josue.santos.dev@gmail.com>
2022-11-01 11:52:43 -0300

    [middleware] (auth)

--- src/libraries/middlewares/auth.ts
+++ src/libraries/middlewares/auth.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import * as jwt from "jsonwebtoken";
+import { user } from "../models/user";
 
 export class Auth {
     async authorization(req: Request, res: Response, next: NextFunction) {
@@ -16,8 +17,12 @@ export class Auth {
         const [, token] = authorization.split(' ')
 
         try {
-            console.log(token)
             await jwt.verify(token, String(process.env.APP_PASS))
+
+            const token_decoded = jwt.decode(token) as user
+
+            req.headers.authorization = token_decoded.email
+
             next()
         } catch (e) {
             console.log(e)
