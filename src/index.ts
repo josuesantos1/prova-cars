1d557a7657bfca6b72e35d9f1ad4804d57199b30
Josué Santos <josue.santos.dev@gmail.com>
2022-11-02 09:53:57 -0300

    change port to 3030

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import { AppDataSource } from "./data-source"
 AppDataSource.initialize()
     .then(() => {
         console.log("[{+}] AppResource initialized")
-        new App().server.listen(3000);
+        new App().server.listen(3030);
     })
     .catch((error) => console.log(error))
